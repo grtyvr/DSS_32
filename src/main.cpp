4d5124064461e9a1aba41948a193d26325490992
Guy Thomas <guy.r.thomas@gmail.com>
2018-07-18 21:50:25 -0700

    Added wifi server code.  Sky Safari can connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,141 @@
+/*
+ESP_32 version of Digital Setting Circles project
+
+Accept connections from SkySafari on port 12
+Send Data back.
+
+Thanks to the folks at ZoetropeLabs for example code to access the AS5048's
+https://github.com/ZoetropeLabs/AS5048A-Arduino
+
+Thanks to Paul Soffgren for the encoder library
+Thanks to Adafruit for being so AWESOME!
+
+ */
+
 #include <Arduino.h>
+#include <WiFi.h>
+
+// Define this if you want to run as an Access Point.  If undefined it will connect to the
+// SSID with the password below....
+
+#define AP
+// uncomment the next line to turn on debugging
+#define DEBUGGING
+
+//char ssid[] = "braapppp"; //  your network SSID (name)
+//char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
+
+const char *apssid = "ESPap";
+const char *appassword = "gofish";
+
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(23);
+
+boolean alreadyConnected = false; // whether or not the client was connected previously
 
 void setup() {
-    // put your setup code here, to run once:
-    pinMode(13, OUTPUT);
-}
+  //Initialize serial
+  Serial.begin(115200);
+  delay(1000);
+
+  #ifdef AP
+    Serial.println("Setting up WiFi Access Point");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(apssid);
+    Serial.print("AP IP Address: ");
+    Serial.println(WiFi.softAPIP());
+  #else
+    // attempt to connect to Wifi network:
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
+
+      // wait 10 seconds for connection:
+      delay(5000);
+    }
+  #endif
+
+  // start the server:
+  server.begin();
+  // you're connected now, so print out the status:
+  #ifdef AP
+  #else
+    printWifiStatus();
+  #endif
+
+} // end setup
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(13, HIGH);
-    delay(50);
-    digitalWrite(13, LOW);
-    delay(50);
+  // wait for a new client:
+  delay(1000);
+
+  WiFiClient thisClient = server.available();
+  // when the client sends the first byte, say hello:
+  while (thisClient) {
+    if (!alreadyConnected) {
+      alreadyConnected = true;
+    }
+    if (thisClient.connected()) {
+      if (thisClient.available() > 0) {
+        // if there are chars to read....
+        // lets print a response and discard the rest of the bytes
+        thisClient.print("000000+");
+        thisClient.print("\t");
+        thisClient.print("000000+");
+        thisClient.print("\r\n");
+        #ifdef DEBUGGING
+          Serial.print("Azimuth tic: ");
+          Serial.print("000000+");
+          Serial.print(" Altitude tic: ");
+          Serial.println("000000+");
+        #endif
+        // discard remaining bytes
+        thisClient.flush();
+      }
+    }
+    else {
+      thisClient.stop();
+      alreadyConnected = false;
+    }
+  }
+}
+
+
+// Calculate Even parity of word
+byte calcEvenParity(word value) {
+  byte count = 0;
+  byte i;
+  // loop through the 16 bits
+  for (i = 0; i < 16; i++) {
+    // if the rightmost bit is 1 increment our counter
+    if (value & 0x1) {
+      count++;
+    }
+    // shift off the rightmost bit
+    value >>=1;
+  }
+  // all odd binaries end in 1
+  return count & 0x1;
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
