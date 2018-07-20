4159d1070eb8ac9eb3a3edf578d31741a7fbd9f2
Guy Thomas <guy.r.thomas@gmail.com>
2018-07-19 21:02:11 -0700

    added encoder, made blink without delay and kept wifi code

--- src/main.cpp
+++ src/main.cpp
@@ -1,358 +1,168 @@
+/*
+ESP_32 version of Digital Setting Circles project
+
+Accept connections from SkySafari on port 12
+Send Data back.
+
+Thanks to the folks at ZoetropeLabs
+https://github.com/ZoetropeLabs/AS5048A-Arduino
+
+Thanks to Paul Stoffregen for the encoder library
+Thanks to Adafruit for being so AWESOME!  And for the graphics libraries.
+
+ */
+
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <Encoder.h>
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
+Encoder myEnc(0,2);
 
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+long oldPosition  = -999;
+unsigned long previousMillis = 0;
+int ledState = LOW;             // ledState used to set the LED
 
-#if (SSD1306_LCDHEIGHT != 32)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
+// Define this if you want to run as an Access Point.  If undefined it will connect to the
+// SSID with the password below....
 
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
-
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
-    }
-    display.display();
-    delay(200);
-
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-        icons[f][XPOS] = random(display.width());
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
-}
+#define AP
+// uncomment the next line to turn on debugging
+#define DEBUGGING
 
 
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
 
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
-  delay(1);
-}
+//char ssid[] = "braapppp"; //  your network SSID (name)
+//char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
 
-void testdrawcircle(void) {
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
+const char *apssid = "ESPap";
+const char *appassword = "gofish";
 
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-    display.display();
-    delay(1);
-    color++;
-  }
-}
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
-void testdrawtriangle(void) {
-  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
+int status = WL_IDLE_STATUS;
 
-void testfilltriangle(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
+WiFiServer server(23);
 
-void testdrawroundrect(void) {
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawrect(void) {
-  for (int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawline() {
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-}
-
-void testscrolltext(void) {
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
-  delay(1);
-
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-}
+boolean alreadyConnected = false; // whether or not the client was connected previously
 
 void setup() {
-    // put your setup code here, to run once:
-    pinMode(13, OUTPUT);
-    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
-// init done
+  pinMode(13, OUTPUT);
+  //Initialize serial
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Basic Encoder Test:");
 
-// Show image buffer on the display hardware.
-// Since the buffer is intialized with an Adafruit splashscreen
-// internally, this will display the splashscreen.
-display.display();
-delay(2000);
+  #ifdef AP
+    Serial.println("Setting up WiFi Access Point");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(apssid);
+    Serial.print("AP IP Address: ");
+    Serial.println(WiFi.softAPIP());
+  #else
+    // attempt to connect to Wifi network:
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
 
-// Clear the buffer.
-display.clearDisplay();
+      // wait 10 seconds for connection:
+      delay(5000);
+    }
+  #endif
 
-// draw a single pixel
-display.drawPixel(10, 10, WHITE);
-// Show the display buffer on the hardware.
-// NOTE: You _must_ call display after making any drawing commands
-// to make them visible on the display hardware!
-display.display();
-delay(2000);
-display.clearDisplay();
+  // start the server:
+  server.begin();
+  // you're connected now, so print out the status:
+  #ifdef AP
+  #else
+    printWifiStatus();
+  #endif
 
-// draw many lines
-testdrawline();
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// draw rectangles
-testdrawrect();
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// draw multiple rectangles
-testfillrect();
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// draw mulitple circles
-testdrawcircle();
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// draw a white circle, 10 pixel radius
-display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
-display.display();
-delay(2000);
-display.clearDisplay();
-
-testdrawroundrect();
-delay(2000);
-display.clearDisplay();
-
-testfillroundrect();
-delay(2000);
-display.clearDisplay();
-
-testdrawtriangle();
-delay(2000);
-display.clearDisplay();
-
-testfilltriangle();
-delay(2000);
-display.clearDisplay();
-
-// draw the first ~12 characters in the font
-testdrawchar();
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// draw scrolling text
-testscrolltext();
-delay(2000);
-display.clearDisplay();
-
-// text display tests
-display.setTextSize(1);
-display.setTextColor(WHITE);
-display.setCursor(0,0);
-display.println("Hello, world!");
-display.setTextColor(BLACK, WHITE); // 'inverted' text
-display.println(3.141592);
-display.setTextSize(2);
-display.setTextColor(WHITE);
-display.print("0x"); display.println(0xDEADBEEF, HEX);
-display.display();
-delay(2000);
-display.clearDisplay();
-
-// miniature bitmap display
-display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-display.display();
-delay(1);
-
-// invert the display
-display.invertDisplay(true);
-delay(1000);
-display.invertDisplay(false);
-delay(1000);
-display.clearDisplay();
-
-// draw a bitmap icon and 'animate' movement
-testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-}
+} // end setup
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(13, HIGH);
-    delay(50);
-    digitalWrite(13, LOW);
-    delay(50);
+  // wait for a new client:
+  //delay(1000);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 500){
+    previousMillis = currentMillis;
+    if (ledState == LOW){
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+  }
+  digitalWrite(13, ledState);
+
+  WiFiClient thisClient = server.available();
+  // when the client sends the first byte, say hello:
+  while (thisClient) {
+    if (!alreadyConnected) {
+      alreadyConnected = true;
+    }
+    if (thisClient.connected()) {
+      if (thisClient.available() > 0) {
+        // if there are chars to read....
+        // lets print a response and discard the rest of the bytes
+        thisClient.print("000000+");
+        thisClient.print("\t");
+        thisClient.print("000000+");
+        thisClient.print("\r\n");
+        #ifdef DEBUGGING
+          Serial.print("Azimuth tic: ");
+          Serial.print("000000+");
+          Serial.print(" Altitude tic: ");
+          Serial.println("000000+");
+        #endif
+        // discard remaining bytes
+        thisClient.flush();
+      }
+    }
+    else {
+      thisClient.stop();
+      alreadyConnected = false;
+    }
+  }
+  
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
+
+
+// Calculate Even parity of word
+byte calcEvenParity(word value) {
+  byte count = 0;
+  byte i;
+  // loop through the 16 bits
+  for (i = 0; i < 16; i++) {
+    // if the rightmost bit is 1 increment our counter
+    if (value & 0x1) {
+      count++;
+    }
+    // shift off the rightmost bit
+    value >>=1;
+  }
+  // all odd binaries end in 1
+  return count & 0x1;
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
