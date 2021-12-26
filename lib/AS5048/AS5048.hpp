95814960303d1e4e4c7f80727a095f796fce76bb
Guy Thomas <guy@grtyvr.com>
2021-12-25 22:51:28 -0800

    added first unit tests

---
+++ lib/AS5048/AS5048.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <SPI.h>
+
+class AS5048A{
+    private:
+        bool _errorFlag;
+        uint8_t _errorValue;
+        uint8_t _cs;
+        float _angle;  // stores the last angle read
+        uint8_t _nullZone = 0;
+
+    SPISettings settings;
+
+    public:
+        /**
+         * @brief Constructor.
+         * 
+         *  @param {uint8_t} arg_cs The pin used for selecting the chip. 
+         *  @param {uint8_t} nullZone The number of ticks to use as a null zone.
+         */
+        AS5048A(uint8_t arg_cs, uint8_t nullZone = 3);
+        /**
+         * @brief Initializer sets up the SPI bus for communications with the chip.  
+         * 
+         * @param clock default value of 1KHz
+         * @param bitOrder default value of MSBFIRST
+         * @param dataMode default value of SPI_MODE1 
+         */
+        void init(uint32_t clock = 1000000, uint8_t bitOrder = MSBFIRST, uint8_t dataMode = SPI_MODE1);
+        /**
+         * Close the SPI bus.  If you need to work with other devices on the same bus that do not 
+         * have the same paramaters be sure to close the bus when you are done with a transaction.
+         */
+        void close();
+        /**
+         * getMagnitude
+         * One of the diagnostic features of the AS5048 is that it can report the magnitude 
+         * of the magnetic field
+         * @return {uint16_t} magnitude.
+         */
+        uint16_t getMagnitude();
+        /**
+         * getAngle
+         * @return {uint16_t} angle as a value in the interval [0,2^14-1].  Rotation counter clockwise 
+         * from the current zero position
+         */
+        uint16_t getAngle();
+        /**
+         * getExpSmoothAngle
+         * @param {float} smoothingFactor
+         * @return {uint16_t} angle as a value in the interval [0,2^14-1].  Rotation counter clockwise
+         * from the current zero position.  This value is caclulated from the last angle read and the 
+         * current angle read by the following formula
+         * new_angle = old_angle*(1 - smoothingFactor) + new_angle*smoothingFactor
+         */
+        uint16_t getExpSmoothAngle(float smoothingFactor);
+        /**
+         * getMeanAngle
+         * @param {int} numSamples
+         * @return {uint16_t} mean value from a sample of numSample angle readings as a value in the 
+         * interval [0,2^14-1].  Rotation counter clockwise from the current zero position.  
+         * This value is the circular mean of the angles read.
+         */
+        uint16_t getMeanAngle(int numSamples);
+        /**
+         * printDiagnostics
+         * Print diagnostic information to the serial port 
+         */
+        void printDiagnostics();
+        /**
+         * getGain
+         * @return {uint8_t} Return the current Gain register value. The gain is stored in the 
+         * first 8 bits of the Diagnostics + Automatic Gain Control register 0x3FFD
+         */
+        uint8_t getGain();
+        /**
+         * getErrors
+         * @return {uint8_t} Return the error flags if set.
+         * 
+         */
+        uint8_t getErrors();
+        
+        bool error();
+    private:
+        uint16_t getDiag();
+        uint8_t calcEvenParity(uint16_t value);
+        uint16_t read(uint16_t registerAddress);
+        uint16_t write(uint16_t registerAddress, uint16_t data);
+};
---
+++ test/test_main.cpp
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+#include <unity.h>
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+void test_led_builtin_pin_number(void) {
+    // update here if schematic changes
+    // TEST_ASSERT_EQUAL(13, );
+}
+
+void test_output_state_high(void) {
+    // display 
+    digitalWrite(22, HIGH); // SCL
+    digitalWrite(21, HIGH); // SDA
+    // Encoders SPI
+//    digitalWrite(19, HIGH); // CIPO
+//    digitalWrite(18, HIGH); // SCK
+//    digitalWrite(23, HIGH); // COPI
+//    digitalWrite(5, HIGH);  // PS-1
+//    digitalWrite(36, HIGH); // PS-2
+    // buttons
+    digitalWrite(15, HIGH); // UP
+    digitalWrite(16, HIGH); // OK
+    digitalWrite(17, HIGH); // DOWN
+
+        // display 
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(22)); // SCL
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(21)); // SDA
+    // Encoders SPI
+//    TEST_ASSERT_EQUAL(HIGH, digitalRead(19)); // CIPO
+//    TEST_ASSERT_EQUAL(HIGH, digitalRead(18)); // SCK
+//    TEST_ASSERT_EQUAL(HIGH, digitalRead(23)); // COPI
+//    TEST_ASSERT_EQUAL(HIGH, digitalRead(5));  // PS-1
+//    TEST_ASSERT_EQUAL(HIGH, digitalRead(36)); // PS-2
+    // buttons
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(15)); // UP
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(16)); // OK
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(17)); // DOWN
+
+}
+
+void test_output_state_low(void) {
+    // display 
+    digitalWrite(22, LOW); // SCL
+    digitalWrite(21, LOW); // SDA
+    // Encoders SPI
+//    digitalWrite(19, LOW); // CIPO
+//    digitalWrite(18, LOW); // SCK
+//    digitalWrite(23, LOW); // COPI
+//    digitalWrite(5, LOW);  // PS-1
+//    digitalWrite(36, LOW); // PS-2
+    // buttons
+    digitalWrite(15, LOW); // UP
+    digitalWrite(16, LOW); // OK
+    digitalWrite(17, LOW); // DOWN
+
+        // display 
+    TEST_ASSERT_EQUAL(LOW, digitalRead(22)); // SCL
+    TEST_ASSERT_EQUAL(LOW, digitalRead(21)); // SDA
+    // Encoders SPI
+//    TEST_ASSERT_EQUAL(LOW, digitalRead(19)); // CIPO
+//    TEST_ASSERT_EQUAL(LOW, digitalRead(18)); // SCK
+//    TEST_ASSERT_EQUAL(LOW, digitalRead(23)); // COPI
+//    TEST_ASSERT_EQUAL(LOW, digitalRead(5));  // PS-1
+//    TEST_ASSERT_EQUAL(LOW, digitalRead(36)); // PS-2
+    // buttons
+    TEST_ASSERT_EQUAL(LOW, digitalRead(15)); // UP
+    TEST_ASSERT_EQUAL(LOW, digitalRead(16)); // OK
+    TEST_ASSERT_EQUAL(LOW, digitalRead(17)); // DOWN
+}
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    pinMode(22, OUTPUT); // SCL
+    pinMode(21, OUTPUT); // SDA
+    // Encoders SPI
+//    pinMode(19, OUTPUT); // CIPO
+//    pinMode(18, OUTPUT); // SCK
+//    pinMode(23, OUTPUT); // COPI
+//    pinMode(5, OUTPUT);  // PS-1
+//    pinMode(36, OUTPUT); // PS-2
+    // buttons
+    pinMode(15, OUTPUT); // UP
+    pinMode(16, OUTPUT); // OK
+    pinMode(17, OUTPUT); // DOWN
+
+    UNITY_BEGIN();    // IMPORTANT LINE!
+    RUN_TEST(test_led_builtin_pin_number);
+
+    pinMode(48, OUTPUT);
+}
+
+uint8_t i = 0;
+uint8_t max_blinks = 5;
+
+void loop() {
+    if (i < max_blinks)
+    {
+        RUN_TEST(test_output_state_high);
+        delay(500);
+        RUN_TEST(test_output_state_low);
+        delay(500);
+        i++;
+    }
+    else if (i == max_blinks) {
+      UNITY_END(); // stop unit testing
+    }
+}
