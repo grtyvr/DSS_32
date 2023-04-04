e9af4e1205125b9cfe5e8352ed0cc0031ff41269
Guy Thomas <guy@grtyvr.com>
2023-04-04 12:26:52 -0700

    display tweaks

---
+++ include/Encoder.hpp
@@ -0,0 +1,33 @@
+#pragma once
+//
+// Encoder
+// ---------------------------------------------------------------------------
+// (c)2023 by GRTYVR. See LICENSE for details.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#include "ArduinoHeaders.hpp"
+
+// Abstract interface for an Encoder object.
+//    
+
+namespace grt {
+class Encoder {
+    public:
+        virtual uint16_t getTics() = 0;
+        virtual uint16_t getMaxTics() = 0;
+};
+}
